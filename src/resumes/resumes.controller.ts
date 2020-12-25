fafc3a255aeee399a0eefd5984319e470d3001ce
Chuong Nguyen <nguyenc123@gmail.com>
2020-12-25 08:55:35 -0500

    modify resumes resource

--- src/resumes/resumes.controller.ts
+++ src/resumes/resumes.controller.ts
@@ -4,37 +4,14 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @ApiTags('Resume')
 @Controller('resumes')
 export class ResumesController {
-
-  @Post()
-  @ApiOperation({
-    summary: 'Create a resume',
-    description: 'Creates an empty resume object that is ready to be built from other endpoints',
-  })
-  createResume() {
-    // Create resume document in mongo
-    return 'Resume id'
-  }
-
-  @Get(':id')
-  @ApiOperation({
-    summary: 'Retrieve a resume',
-    description: 'Retrieves the details of a resume associated with the resume ID that is supplied',
-  })
-  findOneResume(@Param('id') id: string) {
-    return {
-      profile: 'bro'
-    }
-  }
-
   @Get()
   @ApiOperation({
-    summary: 'List all resumes',
-    description: 'Returns a list of resumes in the database',
-
+    summary: 'Retrieve resume',
+    description: 'Retrieves the details of my resume constructed according to the JSONResume.org schema',
   })
-  listAllResumes() {
+  getResume() {
     return {
-      resume: 'bro'
+      profile: 'bro'
     }
   }
 }
